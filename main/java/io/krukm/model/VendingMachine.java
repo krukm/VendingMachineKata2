e5aae23828ae85457431e74c17cdd2774384f08e
Matthew Kruk <matthew.kruk@gmail.com>
2017-10-04 18:28:53 -0400

    add activateCoinReturn to VendingMachine/ test

--- main/java/io/krukm/model/VendingMachine.java
+++ main/java/io/krukm/model/VendingMachine.java
@@ -1,6 +1,8 @@
 package io.krukm.model;
 
 
+import java.util.Stack;
+
 public class VendingMachine {
 
     Display display = new Display();
@@ -19,8 +21,8 @@ public class VendingMachine {
         }
     }
 
-    public void activateCoinReturn() {
-        coinReserve.activateCoinReturn();
+    public Stack<Coin> activateCoinReturn() {
+        return coinReserve.activateCoinReturn();
     }
 
 }
